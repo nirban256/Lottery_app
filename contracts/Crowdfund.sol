6711b11fdea5172f3d509f34f002cb9b193e3830
nirban256 <nirban256@gmail.com>
2022-03-01 12:05:32 +0530

    tested and added smart contract

--- contracts/Crowdfund.sol
+++ contracts/Crowdfund.sol
@@ -1,19 +1,65 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity ^0.8.10;
 
 contract Crowdfund {
-    address manager;
-    uint256 funder;
-    mapping(uint256 => address) donationList;
+    address public manager;
+    uint256 public numOfFunders;
+    uint256 public target;
+    uint256 public deadline;
+    uint256 public raisedAmount;
+    uint256 public minimumContribution;
+    bool public completed = false;
+    mapping(address => uint256) public fundingList;
 
-    constructor() {
+    constructor(
+        uint256 _target,
+        uint256 _deadline,
+        uint256 _minimumContribution
+    ) {
         manager = msg.sender;
+        target = _target;
+        deadline = block.timestamp + _deadline;
+        minimumContribution = _minimumContribution;
     }
 
-    function receivingFunding() public payable {
+    function donate() public payable {
+        require(block.timestamp < deadline, "Funding time limit exceeded");
+        require(
+            msg.value >= minimumContribution,
+            "Transfer amount smaller than minimumContribution limit"
+        );
         require(msg.sender != manager);
-        require(address(msg.sender).balance >= 1 ether, "Not enough balance");
-        require(address(this).balance <= 30 ether, "Target is reached");
-        donationList[funder] = msg.sender;
+        if (fundingList[msg.sender] == 0) {
+            numOfFunders++;
+        }
+        fundingList[msg.sender] += msg.value;
+        raisedAmount += msg.value;
+    }
+
+    function contractBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function withdrawBalance() public {
+        require(
+            block.timestamp > deadline && raisedAmount < target,
+            "Not eligible for refund"
+        );
+        require(fundingList[msg.sender] > 0);
+        address payable user = payable(msg.sender);
+        user.transfer(fundingList[msg.sender]);
+        fundingList[msg.sender] = 0;
+    }
+
+    function transferToOwner() public {
+        require(msg.sender == manager, "You are not the receiver");
+        require(
+            block.timestamp > deadline && raisedAmount >= target,
+            "Not eligible to receive funds"
+        );
+        require(completed == false);
+        address payable receiver = payable(manager);
+        receiver.transfer(raisedAmount);
+        completed = true;
     }
 }
--- fund/src/App.js
+++ fund/src/App.js
@@ -2,7 +2,18 @@ import './index.css';
 
 const App = () => {
   return (
-    <p className='bg-blue-700'>Hello</p>
+    <div className="flex flex-col text-center align-middle justify-center bg-violet-500">
+      <h1 className="text-yellow-400 text-5xl font-bold my-5">
+        Funding for A Cause
+      </h1>
+      <div className="flex  align-middle justify-center">
+        <input type="text" name="description" className="px-1 py-1" placeholder="Enter the cause for raising funding" />
+        <input type="number" name="target" placeholder="Enter the total amount you want to raise" />
+        <input type="number" name="minimum contribution" placeholder="Enter the minimum contribution amount" />
+        <input type="time" name="deadline" placeholder="Enter the time limit for funding" />
+        <button type="submit" className="px-1 py-1 bg-yellow-500 text-white">Submit</button>
+      </div>
+    </div>
   );
 }
 
