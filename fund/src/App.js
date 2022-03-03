2828c1f6d7612b639cdf8dfcaba541375b2a531f
nirban256 <nirban256@gmail.com>
2022-03-03 20:27:37 +0530

    changing the app from crowdfunding to lottery-app

--- fund/src/App.js
+++ fund/src/App.js
@@ -1,17 +1,29 @@
 import './index.css';
+import './App.css';
 
 const App = () => {
   return (
-    <div className="flex flex-col text-center align-middle justify-center bg-violet-500">
-      <h1 className="text-yellow-400 text-5xl font-bold my-5">
+    <div className="text-yellow-400 flex flex-col text-center align-middle justify-center">
+      <h1 className="text-5xl font-bold my-5">
         Funding for A Cause
       </h1>
-      <div className="flex  align-middle justify-center">
-        <input type="text" name="description" className="px-1 py-1" placeholder="Enter the cause for raising funding" />
-        <input type="number" name="target" placeholder="Enter the total amount you want to raise" />
-        <input type="number" name="minimum contribution" placeholder="Enter the minimum contribution amount" />
-        <input type="time" name="deadline" placeholder="Enter the time limit for funding" />
-        <button type="submit" className="px-1 py-1 bg-yellow-500 text-white">Submit</button>
+      <p className="font-bold text-3xl">
+        Contract Balance: 20 Ether
+      </p>
+      <div className="flex flex-col align-middle justify-center">
+        <p className="my-2">
+          Cause for funding: <input type="text" name="description" className="px-1 py-1" placeholder="Enter the cause for raising funding" width={50} />
+        </p>
+        <p className="my-2">
+          Total amount require: <input type="number" name="target" placeholder="Enter the total amount you want to raise" />
+        </p>
+        <p className="my-2">
+          Last date to fund: <input type="time" name="deadline" placeholder="Enter the time limit for funding" />
+        </p>
+        <div>
+          <button type="submit" className="px-1 py-1 mx-2 my-2 bg-yellow-500 text-white">Connect To Metamask</button>
+          <button type="submit" className="px-1 py-1 my-2 bg-yellow-500 text-white">Submit</button>
+        </div>
       </div>
     </div>
   );
