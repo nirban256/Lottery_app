092be3d622be0975fcb0f6113cb209840ad18681
nirban256 <nirban256@gmail.com>
2022-03-06 01:14:14 +0530

    changed frontend to lottery game

--- fund/src/App.js
+++ fund/src/App.js
@@ -1,29 +1,24 @@
 import './index.css';
 import './App.css';
+import Connect from './components/Connect';
 
 const App = () => {
   return (
-    <div className="text-yellow-400 flex flex-col text-center align-middle justify-center">
-      <h1 className="text-5xl font-bold my-5">
-        Funding for A Cause
+    <div className="text-yellow-400 flex flex-col text-center ">
+      <h1 className="text-5xl font-bold my-5 uppercase">
+        Lottery Game
       </h1>
       <p className="font-bold text-3xl">
-        Contract Balance: 20 Ether
+        Prize Money: 20 Ether
       </p>
-      <div className="flex flex-col align-middle justify-center">
-        <p className="my-2">
-          Cause for funding: <input type="text" name="description" className="px-1 py-1" placeholder="Enter the cause for raising funding" width={50} />
-        </p>
-        <p className="my-2">
-          Total amount require: <input type="number" name="target" placeholder="Enter the total amount you want to raise" />
-        </p>
-        <p className="my-2">
-          Last date to fund: <input type="time" name="deadline" placeholder="Enter the time limit for funding" />
-        </p>
-        <div>
-          <button type="submit" className="px-1 py-1 mx-2 my-2 bg-yellow-500 text-white">Connect To Metamask</button>
-          <button type="submit" className="px-1 py-1 my-2 bg-yellow-500 text-white">Submit</button>
-        </div>
+      <div>
+        <button type="submit" className="px-1 py-1 mx-2 my-2 bg-yellow-500 text-white" onClick={async () => {
+          const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+          console.log(accounts);
+        }}>Connect To Metamask</button>
+      </div>
+      <div>
+        <Connect />
       </div>
     </div>
   );
