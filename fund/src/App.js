10439485107085982b34d4a64d4d4f1837176861
nirban256 <nirban256@gmail.com>
2022-03-06 21:56:21 +0530

    added ganache ui for transactions

--- fund/src/App.js
+++ fund/src/App.js
@@ -1,6 +1,5 @@
 import './index.css';
 import './App.css';
-import Connect from './components/Connect';
 
 const App = () => {
   return (
@@ -11,14 +10,15 @@ const App = () => {
       <p className="font-bold text-3xl">
         Prize Money: 20 Ether
       </p>
-      <div>
+      {/*<div>
         <button type="submit" className="px-1 py-1 mx-2 my-2 bg-yellow-500 text-white" onClick={async () => {
           const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
           console.log(accounts);
         }}>Connect To Metamask</button>
-      </div>
+      </div>*/}
       <div>
-        <Connect />
+        <input type="number" value={amount} placeholder="Enter the amount to pay" />
+        <button type="submit">Pay</button>
       </div>
     </div>
   );
